3e28ce44bb01cda3aae19ef2ba0d927c3920c119
marccgrau <marcgrau@outlook.com>
2025-01-20 18:20:20 +0100

    config stuff

--- config.ts
+++ config.ts
@@ -1,11 +1,13 @@
 type Config = {
   includeLeftSidebar: boolean;
   includeRightSidebar: boolean;
+  includeMockMessages: boolean;
 };
 
 const config: Config = {
-  includeLeftSidebar: process.env.NEXT_PUBLIC_INCLUDE_LEFT_SIDEBAR === "true",
-  includeRightSidebar: process.env.NEXT_PUBLIC_INCLUDE_RIGHT_SIDEBAR === "true",
+  includeLeftSidebar: process.env.NEXT_PUBLIC_INCLUDE_LEFT_SIDEBAR === 'false',
+  includeRightSidebar: process.env.NEXT_PUBLIC_INCLUDE_RIGHT_SIDEBAR === 'true',
+  includeMockMessages: process.env.NEXT_PUBLIC_INCLUDE_MOCK_MESSAGES === 'true',
 };
 
 export default config;
